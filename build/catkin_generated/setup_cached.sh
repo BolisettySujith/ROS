131678944983d6ba4dc37d1cdbb2936089ce2363
BolisettySujith <bsujith116@gmail.com>
2022-06-20 13:10:35 +0530

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rassi/Documents/project/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/rassi/Documents/project/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/rassi/Documents/project/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/rassi/Documents/project/src:$ROS_PACKAGE_PATH"
